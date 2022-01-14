1d610d28e86660618d0634af5126aae45ed54910
nakasigit <annas.mulyo@binus.ac.id>
2022-01-14 10:03:05 +0700

    tambahan sout selamat datang dan comment

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
 	public Main() {
 		System.out.println("Hello World");
+		System.out.println("Selamat datang di Pertemuan 7 Mobile Apps"); //tambahan aja
 	}
 	
 	public static void main(String[] args) {
